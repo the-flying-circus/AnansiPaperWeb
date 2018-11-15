87d35cda9d541fb19a0889c98c782a2180a4b586
Grey Golla <bobobalink@gmail.com>
2018-11-15 15:11:03 -0800

    set up command more

--- web/management/commands/import_CrossRef.py
+++ web/management/commands/import_CrossRef.py
@@ -2,3 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 
 from web.models import Article
 from requests import get
+
+
+class Command(BaseCommand):
+    help = 'Import data from CrossRef'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **kwargs):
+        pass

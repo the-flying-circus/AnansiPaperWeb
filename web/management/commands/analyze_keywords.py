8c5123a12e79addd4ed4885ce17ec8615950b41c
Grey Golla <bobobalink@gmail.com>
2018-11-16 07:12:52 -0800

    made keyword extraction suck slightly less

--- web/management/commands/analyze_keywords.py
+++ web/management/commands/analyze_keywords.py
@@ -16,10 +16,12 @@ class Command(BaseCommand):
 
             abstract = article.abstract
             if abstract is not None:
-                r = Rake(max_length=3)
+                r = Rake(max_length=2)
                 r.extract_keywords_from_text(abstract)
-                keywords = r.get_ranked_phrases_with_scores()
+                keywords = r.get_ranked_phrases_with_scores()[:10]
                 for rank, keyword in keywords:
+                    if len(keyword) <= 3:
+                        continue
                     keywordObj, _ = Keyword.objects.get_or_create(keyword=keyword, defaults={'keyword': keyword})
                     KeywordRank.objects.create(rank=rank, keyword=keywordObj, article=article)
                     self.stdout.write("    wrote keyword '{}' (rank {})".format(keyword, rank))

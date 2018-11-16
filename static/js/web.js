740448771dfbb4ba608059e7d8ec876f14a1c577
Haicheng Charles Zhao <czhao39@gmail.com>
2018-11-16 07:32:57 -0500

    Switch to dark color scheme

--- static/js/web.js
+++ static/js/web.js
@@ -41,6 +41,7 @@ function main() {
     svg.append('svg:defs').append('svg:marker')
         .attr('id', 'end-arrow')
         .attr('viewBox', '0 -5 10 10')
+        .attr('fill', 'white')
         .attr('refX', 7)
         .attr('markerWidth', 8)
         .attr('markerHeight', 8)
@@ -58,7 +59,7 @@ function main() {
         .data(links)
         .enter().append("line")
         .attr("stroke-width", 1)
-        .attr("stroke", "black")
+        .attr("stroke", "white")
         .attr("marker-end", "url(#end-arrow)");
 
     const nodeElements = g.selectAll("circle")
@@ -87,7 +88,7 @@ function main() {
     }
 
     function getTextColor(node, neighbors) {
-        return neighbors.includes(node.id) ? "black" : "grey";
+        return neighbors.includes(node.id) ? "white" : "#ddd";
     }
 
     function calcNodeRadius(node) {
@@ -100,7 +101,7 @@ function main() {
     }
 
     function getLinkColor(node, link) {
-        return isNeighborLink(node, link) ? "black" : "grey";
+        return isNeighborLink(node, link) ? "white" : "#ccc";
     }
 
     function getNeighbors(node) {

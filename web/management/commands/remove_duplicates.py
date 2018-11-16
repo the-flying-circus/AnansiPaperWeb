19f422ea52014e06df155f3cab3c0a361f2275dc
Eric Wang <ezwang1@gmail.com>
2018-11-16 08:34:55 -0800

    print additional info on cleanup

--- web/management/commands/remove_duplicates.py
+++ web/management/commands/remove_duplicates.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Count
+from django.core.cache import cache
 
 from web.models import Article, Author
 
@@ -13,9 +14,12 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.removed = 0
         duplicates = Article.objects.values("title").annotate(Count("id")).order_by().filter(id__count__gt=1)
+        all_ids = []
         for item in duplicates:
-            for article in Article.objects.filter(title=item["title"])[1:]:
-                article.delete()
-            self.stdout.write("Removed '{}'".format(item["title"]))
-            self.removed += 1
-        self.stdout.write(self.style.SUCCESS('Cleanup completed! Removed {} object(s).'.format(self.removed)))
+            ids = list(Article.objects.filter(title=item["title"])[1:].values_list("id", flat=True))
+            all_ids += ids
+            self.stdout.write("Removed '{}' ({} times)".format(item["title"], len(ids)))
+            self.removed += len(ids)
+        Article.objects.filter(id__in=all_ids).delete()
+        cache.clear()
+        self.stdout.write(self.style.SUCCESS('Cleanup completed! Removed {} object(s) with {} object(s) remaining.'.format(self.removed, Article.objects.count())))

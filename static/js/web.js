f01662c733ecb4171fc5adb32ac7e57602ec8d04
Haicheng Charles Zhao <czhao39@gmail.com>
2018-11-16 03:32:42 -0500

    Make arrows bigger

--- static/js/web.js
+++ static/js/web.js
@@ -34,8 +34,8 @@ function main() {
         .attr('id', 'end-arrow')
         .attr('viewBox', '0 -5 10 10')
         .attr('refX', 34)
-        .attr('markerWidth', 4)
-        .attr('markerHeight', 4)
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
         .attr('orient', 'auto')
         .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');

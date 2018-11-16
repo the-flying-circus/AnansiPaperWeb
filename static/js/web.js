0e4692a4fc261a598bed02e19ebca3369ae812b1
Haicheng Charles Zhao <czhao39@gmail.com>
2018-11-16 03:37:29 -0500

    Fix arrowheads

--- static/js/web.js
+++ static/js/web.js
@@ -33,9 +33,9 @@ function main() {
     svg.append('svg:defs').append('svg:marker')
         .attr('id', 'end-arrow')
         .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 34)
-        .attr('markerWidth', 6)
-        .attr('markerHeight', 6)
+        .attr('refX', 22)
+        .attr('markerWidth', 8)
+        .attr('markerHeight', 8)
         .attr('orient', 'auto')
         .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');
